2e408d8dced77a069bbb4d367621ed7562d760cb
cn <cn@mycrobase.de>
2018-11-30 14:07:58 +0100

    examples: improve demo

--- examples/demo.rb
+++ examples/demo.rb
@@ -1,8 +1,20 @@
 
 require 'zeebe/client'
 
-stub = Zeebe::Client::GatewayProtocol::Gateway::Stub.new('localhost:26500', :this_channel_is_insecure)
+zeebe_client = Zeebe::Client::GatewayProtocol::Gateway::Stub.new('localhost:26500', :this_channel_is_insecure)
 
-puts stub.topology(Zeebe::Client::GatewayProtocol::TopologyRequest.new).inspect
+topology = zeebe_client.topology(Zeebe::Client::GatewayProtocol::TopologyRequest.new)
 
-puts stub.list_workflows(Zeebe::Client::GatewayProtocol::ListWorkflowsRequest.new).inspect
+puts "Zeebe topology:"
+puts "  Cluster size: #{topology.clusterSize}"
+puts "  Replication factor: #{topology.replicationFactor}"
+puts "  Brokers:"
+
+topology.brokers.each do |b|
+  puts "    - id: #{b.nodeId}, address: #{b.host}:#{b.port}, partitions: #{b.partitions.map { |p| p.partitionId }}"
+end
+
+workflows = zeebe_client.list_workflows(Zeebe::Client::GatewayProtocol::ListWorkflowsRequest.new).workflows
+
+puts ""
+puts "Zeebe workflows: #{workflows.inspect}"
