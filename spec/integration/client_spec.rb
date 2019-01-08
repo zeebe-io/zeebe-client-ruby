dbb57b2d28d3c653cb5ccf2d5f32f4ad42384ffd
cn <cn@mycrobase.de>
2019-01-08 10:33:59 +0100

    client: add integration test

---
+++ spec/integration/client_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+# do some basic smoke testing with the Zeebe::Client against a real Zeebe gateway port,
+# the aim is not to test all possible procedures available via GRPC
+describe Zeebe::Client do
+  let(:client) do
+    Zeebe::Client::GatewayProtocol::Gateway::Stub.new('localhost:26500', :this_channel_is_insecure)
+  end
+
+  it 'has topology' do
+    topology = client.topology(Zeebe::Client::GatewayProtocol::TopologyRequest.new)
+    expect(topology.clusterSize).to eq(1)
+    expect(topology.replicationFactor).to eq(1)
+  end
+
+  it 'lists workflows' do
+    workflows = client.list_workflows(Zeebe::Client::GatewayProtocol::ListWorkflowsRequest.new).workflows
+    expect(workflows).to eq([])
+  end
+end
