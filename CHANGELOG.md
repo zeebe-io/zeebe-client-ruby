4d89775b19743aa8437b4b0f4d5b635a2ffd337a
cn <cn@mycrobase.de>
2019-01-25 14:39:32 +0100

    gem: release 0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## 0.2.0
+## 0.2.0 (January 25, 2019)
 
 OTHER:
 
--- lib/zeebe/client/version.rb
+++ lib/zeebe/client/version.rb
@@ -1,6 +1,6 @@
 
 module Zeebe
   module Client
-    VERSION = '0.1.0'.freeze
+    VERSION = '0.2.0'.freeze
   end
 end
