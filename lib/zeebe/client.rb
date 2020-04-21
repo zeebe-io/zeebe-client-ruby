6a06783e6cbb29458767f8d46d406cdf4ac31221
Andy Rusterholz <andy@rusterholz.org>
2020-04-21 09:01:03 -0600

    gem: create modules prior to requiring proto/gateway_pb
    create modules prior to requiring proto/gateway_pb (#47)

--- lib/zeebe/client.rb
+++ lib/zeebe/client.rb
@@ -1,9 +1,8 @@
-
-require 'zeebe/client/proto/gateway_pb'
-require 'zeebe/client/proto/gateway_services_pb'
-require 'zeebe/client/version'
-
 module Zeebe
   module Client
   end
 end
+
+require 'zeebe/client/proto/gateway_pb'
+require 'zeebe/client/proto/gateway_services_pb'
+require 'zeebe/client/version'
